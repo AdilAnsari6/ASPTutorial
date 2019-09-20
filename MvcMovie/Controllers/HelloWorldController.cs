a37d420e49ac9e5c8755f10d6172263383f78f44
Unknown <adilabbas6@yahoo.com>
2019-09-20 11:30:12 -0400

    initial

---
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // 
+        // GET: /HelloWorld/
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // 
+        // GET: /HelloWorld/Welcome/ 
+
+        public string Welcome(string name, int numtimes = 1)
+        {
+
+            ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = numtimes;
+
+            return View();
+        }
+    }
+}
